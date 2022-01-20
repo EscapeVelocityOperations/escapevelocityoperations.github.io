fcd4639c7c3db44ec873bbd07fe3b491d66d1659
Eric Cestari <eric@cstar.io>
2022-01-20 16:55:34 +0100

    initial commit

---
+++ build-github.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git checkout --orphan gh-pages
+echo "Build started"
+FOLDER=build
+
+git  --work-tree  $FOLDER add  --all
+git  --work-tree  $FOLDER commit  -m  gh-pages
+Pushing to gh-pages..
+git  push  origin  HEAD:gh-pages  --force
+rm  -r  $FOLDER
+git  checkout  -f  master
+git  branch  -D  gh-pages
+Successfully deployed check your setting
+git  --work-tree  $FOLDER add  --all
+git  --work-tree  $FOLDER commit  -m  gh-pages
+echo Pushing to gh-pages..
+git  push  origin  HEAD:gh-pages  --force
+rm  -r  $FOLDER
+git  checkout  -f  master
+git  branch  -D  gh-pages
+echo Successfully deployed check your setting
