db465655cf6007b85482a421ac810bb368004367
Eric Cestari <eric@cstar.io>
2022-01-20 17:21:22 +0100

    update deploy

--- build-github.sh
+++ build-github.sh
@@ -6,14 +6,6 @@ FOLDER=build
 
 git  --work-tree  $FOLDER add  --all
 git  --work-tree  $FOLDER commit  -m  gh-pages
-Pushing to gh-pages..
-git  push  origin  HEAD:gh-pages  --force
-rm  -r  $FOLDER
-git  checkout  -f  master
-git  branch  -D  gh-pages
-Successfully deployed check your setting
-git  --work-tree  $FOLDER add  --all
-git  --work-tree  $FOLDER commit  -m  gh-pages
 echo Pushing to gh-pages..
 git  push  origin  HEAD:gh-pages  --force
 rm  -r  $FOLDER
